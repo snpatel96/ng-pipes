1e708fc73e6c7141bc047eec731a2fc3d93d6bdf
snehal patel <snpatel.dev@gmail.com>
2022-12-17 14:29:44 -0500

    #1 built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name?: string;
+  paymentDate?: string;
+  amount?: number;
+  height?: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+  };
+
+  onNameChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.name = value;
+  }
+
+  onDateChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.paymentDate = value;
+  }
+
+  onAmountChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.height = parseFloat(value);
+  }
 }
